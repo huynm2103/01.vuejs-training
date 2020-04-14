564a24aa5a5c72ccadc8691a79f71d35a9d25206
huy nguyen <huynm2103@gmail.com>
2020-04-15 01:31:14 +0900

    completed 02

--- 02.first-project/app.js
+++ 02.first-project/app.js
@@ -4,20 +4,58 @@ new Vue({
 		playerHealth: 100,
 		monsterHealth: 100,
 		gameIsRunning: false,
+		turns: [],
 	},
 	methods: {
 		startGame: function () {
 			this.gameIsRunning = true;
 			this.playerHealth = 100;
 			this.monsterHealth = 100;
+			this.turns = [];
 		},
 		attack: function () {
-			this.monsterHealth -= this.calculateDamge(3, 10);
-			this.playerHealth -= this.calculateDamge(3, 10);
+			let playerDmg = this.calculateDamge(3, 10);
+			this.monsterHealth -= playerDmg;
+			this.turns.unshift({
+				isPLayer: true,
+				text: 'PLayer hits monster for ' + playerDmg,
+			});
+
+			let monsterDmg = this.calculateDamge(3, 10);
+			this.playerHealth -= monsterDmg;
+			this.turns.unshift({
+				isPLayer: false,
+				text: 'Monster hits player for ' + monsterDmg,
+			});
 			if (this.checkResult()) this.gameIsRunning = false;
 		},
-		specialAttack: function () {},
-		heal: function () {},
+		specialAttack: function () {
+			let playerDmg = this.calculateDamge(10, 20);
+			this.monsterHealth -= playerDmg;
+			this.turns.unshift({
+				isPLayer: true,
+				text: 'PLayer hits monster for ' + playerDmg,
+			});
+
+			let monsterDmg = this.calculateDamge(10, 20);
+			this.playerHealth -= monsterDmg;
+			this.turns.unshift({
+				isPLayer: false,
+				text: 'Monster hits player for ' + monsterDmg,
+			});
+			if (this.checkResult()) this.gameIsRunning = false;
+		},
+		heal: function () {
+			if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+        this.turns.unshift({
+          isPLayer: true,
+          text: 'Player got heal for 10'
+        });
+			} else {
+				this.playerHealth = 100;
+			}
+		},
 		stopGame: function () {
 			this.gameIsRunning = false;
 		},
