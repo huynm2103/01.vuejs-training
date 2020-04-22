f15ae66b61cf1948622fa5a390d559e243230209
huy nguyen <huynm2103@gmail.com>
2020-04-23 01:59:52 +0900

    try to create directive

--- 06.custom-directive/src/App.vue
+++ 06.custom-directive/src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Build-in Directives</h1>
+        <p v-text="'v-text'"></p>
+        <p v-html="'v-html'"></p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-highlight:background.delayed="'green'">Color this!</p>
+        <p v-highlight="'green'">Color this!</p>
+        <p v-local-highlight:background.delayed="'red'">Local Color this!</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  directives: {
+    "local-highlight": {
+      // eslint-disable-next-line no-unused-vars
+      bind(el, binding, vnode) {
+        let delay = 0;
+        if (binding.modifiers["delayed"]) {
+          delay = 3000;
+        }
+
+        setTimeout(() => {
+          if (binding.arg == "background") {
+            el.style.backgroundColor = binding.value;
+          } else {
+            el.style.color = binding.value;
+          }
+        }, delay);
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- 06.custom-directive/src/main.js
+++ 06.custom-directive/src/main.js
@@ -3,6 +3,28 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive('highlight', {
+  // eslint-disable-next-line no-unused-vars
+  bind(el, binding, vnode) {
+    // el.style.backgroundColor = binding.value;
+    let delay = 0;
+    if(binding.modifiers['delayed']) {
+      delay = 3000;
+    }
+    // console.log(binding)
+    // console.log(el)
+    // console.log(vnode)
+
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
+  }
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
